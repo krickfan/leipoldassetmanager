30f4b83c1ab9b8ed7dc79d1f6944b36ceb65c5ac
Frank Krickhahn <krickhahn.frank@gmail.com>
2024-06-27 17:12:27 +0200

    Login screen changed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import './style.css'
 import App from './App.vue'
 import router from './router';
 
+/*
+import "../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../assets/libs/simplebar/dist/simplebar.min.js";
+import "../assets/js/theme/theme.js";
+
+import "../assets/iconify/iconify-icon.min.js";
+*/
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
